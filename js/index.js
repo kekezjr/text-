57375d868bcc713482f6da98700a0da7d42b588c
zhaojingru <327256087.qq.com>
2019-01-07 23:07:09 +0800

    '完成音频'

--- js/index.js
+++ js/index.js
@@ -5,23 +5,31 @@
 window.onload = function(){
 //    获取dom元素
 //    头部区域的获取
-    var headerLiNodes = document.querySelectorAll('.nav li');
-    var headerArrowNode = document.querySelector('.arrow');
-    var headerUpNodes = document.querySelectorAll('.nav li .up');
+  var headerLiNodes = document.querySelectorAll('.nav li');
+  var headerArrowNode = document.querySelector('.arrow');
+  var headerUpNodes = document.querySelectorAll('.nav li .up');
 
     //内容区域的获取
-    var contentUlNodes = document.querySelector('.contentMain');
-    var contentNode = document.querySelector('.content');
-    var navLiNodes = document.querySelectorAll('.navList li');
+  var contentUlNodes = document.querySelector('.contentMain');
+  var contentNode = document.querySelector('.content');
+
+  //侧边导航栏
+  var navLiNodes = document.querySelectorAll('.navList li');
+
+    //音乐
+  var musicNode = document.querySelector('.music');
+  var musicIconNode = document.querySelector('.music-icon');
 
     //内容区域设置的变量
-    var contentHeight = contentNode.offsetHeight;
-    var nowIndex = 0;
-    var wheelTimer = null;
+  var contentHeight = contentNode.offsetHeight;
+  var nowIndex = 0;
+  var wheelTimer = null;
+
+
 
     //头部函数
-    header();
-    function header(){
+  header();
+  function header(){
 //    初始化第一个headerUpNodes的宽度为100%
         headerUpNodes[0].style.width = '100%';
 //    初始化小箭头来到第一个li下
@@ -39,84 +47,82 @@ window.onload = function(){
     }
 
 //    内容区域的滚轮事件
-    content();
-    function content(){
-        document.onmousewheel = wheel;
-        document.addEventListener('DOMMouseScroll',wheel);
+  content();
+  function content(){
+    document.onmousewheel = wheel;
+    document.addEventListener('DOMMouseScroll',wheel);
 
-        function wheel(event) {
-            event = event || window.event;
+    function wheel(event) {
+      event = event || window.event;
             //函数反斗 防止函数多次调用  优化函数性能  规定时间内调用的函数还有最后一次生效
-            clearTimeout(wheelTimer);
-            wheelTimer = setTimeout(function(){
-                var flag = '';
-                if (event.wheelDelta) {
-                    //ie/chrome
-                    if (event.wheelDelta > 0) {
-                        flag = 'up';
-                    } else {
-                        flag = 'down'
-                    }
-                } else if (event.detail) {
-                    //firefox
-                    if (event.detail < 0) {
-                        flag = 'up';
-                    } else {
-                        flag = 'down'
-                    }
-                }
-                switch (flag) {
-                    case 'up' :
-                        //范围限定
-                        if(nowIndex > 0){
-                            nowIndex--;
-                            // console.log('up');
-                            move(nowIndex);
-                        }
-                        break;
-                    case 'down' :
-                        if(nowIndex < 4){
-                            nowIndex++;
-                            move(nowIndex);
-                        }
-                        break;
-                }
-            },100);
+      clearTimeout(wheelTimer);
+      wheelTimer = setTimeout(function(){
+        var flag = '';
+        if (event.wheelDelta) {
+          //ie/chrome
+          if (event.wheelDelta > 0) {
+            flag = 'up';
+          } else {
+            flag = 'down'
+          }
+        } else if (event.detail) {
+          //firefox
+          if (event.detail < 0) {
+            flag = 'up';
+          } else {
+            flag = 'down'
+          }
+        }
+        switch (flag) {
+          case 'up' :
+            //范围限定
+            if(nowIndex > 0){
+              nowIndex--;
+              // console.log('up');
+              move(nowIndex);
+            }
+            break;
+          case 'down' :
+            if(nowIndex < 4){
+              nowIndex++;
+              move(nowIndex);
+            }
+            break;
+        }
+      },100);
 
             //禁止默认行为
-            event.preventDefault && event.preventDefault();
-            return false;
-        }
-
+      event.preventDefault && event.preventDefault();
+      return false;
     }
 
+  }
+
 //    调整窗口的大小事件
-    window.onresize = function(){
+  window.onresize = function(){
         headerArrowNode.style.left = headerLiNodes[nowIndex].getBoundingClientRect().left +  headerLiNodes[nowIndex].offsetWidth / 2 - headerArrowNode.offsetWidth / 2 + 'px';
         //让内容区的ul跟着移动
         contentUlNodes.style.top = -nowIndex * contentNode.offsetHeight + 'px';
     }
 
 //    封装函数(公共的区域)
-    function move(nowIndex){
+  function move(nowIndex){
 
         //默认清空所有的width为0
-        for(var j = 0;j < headerUpNodes.length; j++){
+    for(var j = 0;j < headerUpNodes.length; j++){
             headerUpNodes[j].style.width = '0';
-            // navLiNodes[j].className = '';
         }
         //手动设置为100%；
-        headerUpNodes[nowIndex].style.width = '100%';
+     headerUpNodes[nowIndex].style.width = '100%';
         //   让小箭头去指定的li的位置下
-        headerArrowNode.style.left = headerLiNodes[nowIndex].getBoundingClientRect().left +  headerLiNodes[nowIndex].offsetWidth / 2 - headerArrowNode.offsetWidth / 2 + 'px';
+     headerArrowNode.style.left = headerLiNodes[nowIndex].getBoundingClientRect().left +  headerLiNodes[nowIndex].offsetWidth / 2 - headerArrowNode.offsetWidth / 2 + 'px';
 
         //让内容区的ul跟着移动
-        contentUlNodes.style.top = -nowIndex * contentHeight + 'px';
+     contentUlNodes.style.top = -nowIndex * contentHeight + 'px';
 
     //    侧标导航
-    //     navLiNodes[nowIndex].style.className = 'active';
+
     }
-    // move(3);
 
 //    第一屏区域的代码
     homeHandle();
@@ -336,7 +342,19 @@ window.onload = function(){
     }
 
 
+//头部音乐
+  musicNode.onclick = function(){
+    if(musicIconNode.paused){
+    //  说明当前音乐暂停，点击播放
+      musicIconNode.play();
+      this.style.backgroundImage = 'url("./img/musicon.gif")';
+    }else{
+      //  说明当前音乐是播放，点击静音
+      musicIconNode.pause();
+      this.style.backgroundImage = 'url("./img/musicoff.gif")';
+    }
 
+  }
 
 
 
